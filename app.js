6a4e487c177cf6ab99d433aca0ac90006ac73fa3
Chry1911 <cortese.christian@libero.it>
2018-09-18 21:16:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ client.on('ready', () => {
 });
 
 /**
-* Il primo evento serve per comunicare in chat generale chi � arrivato come nuovo utente.
+* Il primo evento serve per comunicare in chat generale chi è arrivato come nuovo utente.
 **/
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'chat-community🌍');
+  const channel = member.guild.channels.find(ch => ch.name === 'chat-communityðŸŒ');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
@@ -48,7 +48,7 @@ client.on('message', message=> {
 			.addField('$unwarna', "Elimina i warn mandati allo user, si prega di menzionare lo user con @nome")
 			.addField('$banna', "Banna lo user dal server, si prega di menzionare lo user con @nome")
 			.addField('$kikka', "Kikka lo user fuori dal server, si prega di menzionare lo user con @nome")
-			.addField('$magic', "Comando per insultare magic, lui � sempre nei nostri pensieri, gli vogliamo bene")
+			.addField('$magic', "Comando per insultare magic, lui è sempre nei nostri pensieri, gli vogliamo bene")
 			.setFooter('Requested By ' + message.author.tag)
 			
 			message.channel.send(HelpCommand);
@@ -231,4 +231,3 @@ client.on('message', message => { //Message Event | Listener
 });
 
 client.login(process.env.BOT_TOKEN);
-//client.login('NDg4MDIxODM3NjI4OTY0ODY1.Dn1URQ.xo4ZGvSNjyjsYn83RZcfeJuXew8');
