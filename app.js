0ba7bedcf27ae6b12f0c700166deea8cd3659647
Christian Cortese <cortese.christian@libero.it>
2019-06-19 09:32:18 +0200

    aggiornamento bot sfollati su github

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-//Discord.js Version 12.0.0 MASTER
+ //Discord.js Version 12.0.0 MASTER
 //Npm Discord.js Pakage
 const Discord = require('discord.js'); 
 const client = new Discord.Client();
 const moment = require("moment");
+const YouTube = require('simple-youtube-api');
+//const apiYT = 'AIzaSyDN_vfEa-l5NOxgUlhEzc-oz_wCGJ7P7Hk';
+const apiYT = process.env.YT_API;
+const yt = require('ytdl-core');
+const binaries = require('ffmpeg-binaries');
+const ffmpeg = require('ffmpeg');
+//const opus = require('opusscript');
+//var opus = require('node-opus');
+
 
 let PREFIX = '$' //Prefix Can Be Any 
+let queue = {};
 
 
 client.on('ready', () => {
   console.log('I am ready!');
   console.log('Connected');
-  console.log('Logged on the SfoLLaTi Discord Server');
+  console.log('Logged on the Sfollati Gaming Clan Discord Server');
   console.log('We moderate the people in this server');
 });
 
@@ -21,12 +31,12 @@ client.on('ready', () => {
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'entra_esci');
+  const channel = member.guild.channels.find(ch => ch.name === '♻entra_esci♻');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`Benvenuto negli SfoLLaTi, ${member}`);
-  member.addRole(member.guild.roles.find(role => role.name === "Utente non ancora del clan"));
+  channel.send(`Benvenuto nel nostro server, ${member}`);
+  member.addRole(member.guild.roles.find(role => role.name === "Nuovo membro"));
 });
 
 
@@ -35,7 +45,7 @@ client.on('guildMemberAdd', member => {
 **/
 client.on('guildMemberRemove', member =>{
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'entra_esci');
+  const channel = member.guild.channels.find(ch => ch.name === '♻entra_esci♻');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
@@ -61,11 +71,40 @@ client.on('message', message=> {
 			.addField('$unwarna', "Elimina i warn mandati allo user, si prega di menzionare lo user con @nome")
 			.addField('$banna', "Banna lo user dal server, si prega di menzionare lo user con @nome")
 			.addField('$kikka', "Kikka lo user fuori dal server, si prega di menzionare lo user con @nome")
-			.addField('$magic', "Comando per insultare magic, lui è sempre nei nostri pensieri, gli vogliamo bene")
+      		.addField('$mention', "Manda un messaggio a tutti i moderatori e fondatori del server, mi raccomando di taggare il ruolo @FOUNDER @ADMIN")
+      		.addField('$queue', "Comando per mettere in coda la musica, serve per creare la playlist, primo comando da eseguire")
+      		.addField('$add', "Aggiunge una canzone alla playlist copiando url di youtube")
+      		.addField('$play', "Comando per la musica, seguito da nome canzone per riprodurre il brano desiderato")
+      		.addField('$skip', "Comando per la skippare la musica")
+      		.addField('$stop', "Comando per stoppare la musica")
+      		.addField('$volume', "Comando per il volume della musica")
+      		.addField('$np', "Comando per togliere la playlist")
+      		.addField('$pause', "Comando per mettere in pausa la musica")
+      		.addField('$resume', "Comando per la recuperare la musica")
+            .addField('$report', "Comando per reportare la condotta negativa di un utente")
+            .addField("$private", "Comando per mandare un messaggio privato a tutti gli utenti del server")
+			
 			.setFooter('Requested By ' + message.author.tag)
 			
 			message.channel.send(HelpCommand);
 		}
+});
+
+
+/**
+* Comando per reportare l'utente scrivendo nella chat-blacklist
+**/
+
+client.on('message', message => {
+    if(!message.guild) return;
+
+    if(message.content.startsWith(PREFIX + 'report')){
+      const now = new Date();
+      const user = message.mentions.users.first();
+      const channel = client.channels.find("name", "🏴black-list🏴");
+
+      channel.send(`E' stato reportato l'utente, ` + user +  `in data:` + now);
+    }
 });
 
 /**
@@ -85,6 +124,21 @@ client.on('message', message => {
 				userToSend.send(messageToSend);
 				message.channel.send("L'utente ha ricevuto un warn dal bot per inattivita'");
 		  }
+});
+
+
+
+
+/**
+* Messaggio per tutti gli utenti del server
+**/
+client.on('message', message => {
+  if (message.guild && message.content.startsWith(PREFIX + 'private')) {
+    let text = "Ciao visto che sei nella nostra community collegati nelle stanze per socializzare, ti aspettiamo negli SGC :) ";
+    message.guild.members.forEach(member => {
+      if (member.id != client.user.id && !member.user.bot) member.send(text);
+    });
+  }
 });
 
 /**
@@ -180,6 +234,30 @@ client.on('message', message => {
   }
 });
 
+
+/**
+* Contatta tutti i mod e founders del server
+**/
+client.on("message", message => {
+
+    if (message.content.startsWith(PREFIX + 'mention')) {
+    
+        
+
+        const myRole = message.guild.roles.find(role => role.name === "🔱 FONDATORE 🔱");
+        const myRole2 = message.guild.roles.find(role => role.name === "🛡️ ADMIN 🛡️");
+
+        
+        for (const member of message.guild.members.values()) {
+		    if (member.roles.has(myRole.id || member.roles.has(myRole2.id))) {
+		        //member.user.send("Ho bisogno di voi, per un aiuto");
+		        member.user.send(`Ho bisogno di voi, per un aiuto, ${message.author}`);
+		    }
+		}
+
+    }
+});
+
 /**
 * Restituisce tutte le info dell'utente richieste
 **/
@@ -243,6 +321,167 @@ client.on('message', message => { //Message Event | Listener
     }
 });
 
+/*
+**
+tentiamo di riprodurre la musica
+*/
+client.on('message', message => {
+	if (message.content.startsWith(PREFIX + 'play')) {
+		if(queue[message.guild.id] === undefined) return
+			message.channel.sendMessage('Aggiungi delle canzoni alla coda prima di eseguirle');
+		if(!message.guild.voiceConnection) 
+			return commands.join(message).then(() =>
+				commands.play(message));
+		if (queue[message.guild.id].playing) return message.channel.sendMessage('Gia in esecuzione');
+		let dispatcher;
+		queue[message.guild.id].playing = true;
+
+		console.log(queue);
+		(function play(song) {
+			console.log(song);
+			if (song === undefined) return message.channel.sendMessage('La coda è vuota').then(() => {
+				queue[message.guild.id].playing = false;
+				message.member.voiceChannel.leave();
+			});
+			message.channel.sendMessage(`Playing: **${song.title}** as requested by: **${song.requester}**`);
+			dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : 1 });
+				let collector = message.channel.createCollector(m => m);
+				collector.on('message', m => {
+				if (m.content.startsWith(PREFIX + 'pause')) {
+					message.channel.sendMessage('paused').then(() => {dispatcher.pause();});
+				} else if (m.content.startsWith(PREFIX + 'resume')){
+					message.channel.sendMessage('resumed').then(() => {dispatcher.resume();});
+				} else if (m.content.startsWith(PREFIX + 'skip')){
+					message.channel.sendMessage('skipped').then(() => {dispatcher.end();});
+				} else if (m.content.startsWith('volume+')){
+					if (Math.round(dispatcher.volume*50) >= 100) return message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
+					message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith('volume-')){
+					if (Math.round(dispatcher.volume*50) <= 0) return message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
+					message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith(PREFIX + 'time')){
+					message.channel.sendMessage(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+				}
+}			);
+			dispatcher.on('end', () => {
+				collector.stop();
+				play(queue[message.guild.id].songs.shift());
+			});
+			dispatcher.on('error', (err) => {
+				return message.channel.sendMessage('error: ' + err).then(() => {
+					collector.stop();
+					play(queue[message.guild.id].songs.shift());
+				});
+			});	
+		})(queue[message.guild.id].songs.shift());
+	}
+});
+
+
+/*
+* Creiamo la coda di riproduzione delle canzoni
+*/
+client.on('message', message => {
+	if (message.content.startsWith(PREFIX + 'queue')) {
+		if (queue[message.guild.id] === undefined) return message.channel.sendMessage(`Add some songs to the queue first with $add`);
+		let tosend = [];
+		queue[message.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
+		message.channel.sendMessage(`__**${message.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
+	}
+});
+
+/*
+* Creiamo il comando per aggiungere una canzone alla playlist
+*/
+
+client.on('message', message => {
+	if(message.content.startsWith(PREFIX + 'add')){
+		let url = message.content.split(' ')[1];
+		if (url == '' || url === undefined) return message.channel.sendMessage(`You must add a YouTube video url, or id after $add`);
+		yt.getInfo(url, (err, info) => {
+			if(err) return message.channel.sendMessage('Invalid YouTube Link: ' + err);
+			if (!queue.hasOwnProperty(message.guild.id)) queue[message.guild.id] = {}, queue[message.guild.id].playing = false, queue[message.guild.id].songs = [];
+			queue[message.guild.id].songs.push({url: url, title: info.title, requester: message.author.username});
+			message.channel.sendMessage(`added **${info.title}** to the queue`);
+		});
+	}
+});
+
+/*
+* Comando per far joinare qualcuno nel canale vocale
+*/
+client.on('message', message =>{
+	if(message.content.startsWith(PREFIX + 'join')){
+		return new Promise((resolve, reject) => {
+			const voiceChannel = message.member.voiceChannel;
+			if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply('Non sono stato in grado di connettermi al tuo canale vocale...');
+			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+		});
+	}
+});
+
+const commands = {
+	'play': (message) => {
+		if (queue[message.guild.id] === undefined) return message.channel.sendMessage(`Aggiungi alcune canzoni col comando $add`);
+		if (!message.guild.voiceConnection) return commands.join(message).then(() => commands.play(message));
+		if (queue[message.guild.id].playing) return message.channel.sendMessage('Gia in esecuzione');
+		let dispatcher;
+		queue[message.guild.id].playing = true;
+
+		console.log(queue);
+		(function play(song) {
+			console.log(song);
+			if (song === undefined) return message.channel.sendMessage('La coda è vuota').then(() => {
+				queue[message.guild.id].playing = false;
+				message.member.voiceChannel.leave();
+			});
+			message.channel.sendMessage(`Playing: **${song.title}** as requested by: **${song.requester}**`);
+			dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : 1 });
+			let collector = message.channel.createCollector(m => m);
+			collector.on('message', m => {
+				if (m.content.startsWith(PREFIX + 'pause')) {
+					message.channel.sendMessage('paused').then(() => {dispatcher.pause();});
+				} else if (m.content.startsWith(PREFIX + 'resume')){
+					message.channel.sendMessage('resumed').then(() => {dispatcher.resume();});
+				} else if (m.content.startsWith(PREFIX  + 'skip')){
+					message.channel.sendMessage('skipped').then(() => {dispatcher.end();});
+				} else if (m.content.startsWith('volume+')){
+					if (Math.round(dispatcher.volume*50) >= 100) return message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
+					message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith('volume-')){
+					if (Math.round(dispatcher.volume*50) <= 0) return message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
+					message.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith(PREFIX + 'time')){
+					message.channel.sendMessage(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+				}
+			});
+			dispatcher.on('end', () => {
+				collector.stop();
+				play(queue[message.guild.id].songs.shift());
+			});
+			dispatcher.on('error', (err) => {
+				return message.channel.sendMessage('error: ' + err).then(() => {
+					collector.stop();
+					play(queue[message.guild.id].songs.shift());
+				});
+			});
+		})(queue[message.guild.id].songs.shift());
+},
+	'join' : (message) => {
+		return new Promise((resolve, reject) => {
+			const voiceChannel = message.member.voiceChannel;
+			if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply('Non sono stato in grado di connettermi al tuo canale vocale');
+			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+
+		});
+	}
+}
 
 client.login(process.env.BOT_TOKEN);
+
+
 
